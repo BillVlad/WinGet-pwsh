09939e011c9b5b475f245cc08c9c16db8c2cb887
BillVLad <vladshevchenko29@gmail.com>
2021-05-30 19:59:21 +0300

    Добавил интерактивную установку софта. (P.S. очистил soft.txt... кпц)

--- inst.ps1
+++ inst.ps1
@@ -14,23 +14,32 @@ function InstallSoftware {
     }
 }
 
-$decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
-if ($decision -eq 0) {
-    echo 'Installing software...'
-    Start-Sleep -s 2
-    InstallSoftware
-    Host-clear
+
+$inter = $Host.UI.PromptForChoice('Silent/Interactive', 'Do you want a silent installation?', $choices, 1)
+if ($inter -eq 0) {
+    foreach($line in [System.IO.File]::ReadLines("soft.txt")) {
+           winget install -i $line
+    }
 } else {
-    # Source file location
-    $source = 'https://github.com/microsoft/winget-cli/releases/download/v1.0.11451/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle'
-    # Destination to save the file
-    $our = 'c:\winget.appxbundle'
-    Invoke-WebRequest -Uri $source -OutFile $out
-    echo 'We download WinGet'
-    Start-Sleep -Seconds 3
-    Add-AppxPackage $out
-    Remove-Item $out
-    clear | echo 'Installing software...'
-    InstallSoftware
-    Host-clear
+    Interactive
+}
+
+function Interactive {
+    $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+        if ($decision -eq 0) {
+            echo 'Installing software...'
+            Start-Sleep -s 2
+            InstallSoftware
+            Host-clear
+        } else {
+            # Source file location
+            $source = 'https://github.com/microsoft/winget-cli/releases/download/v1.0.11451/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle'
+            Invoke-WebRequest -Uri $source -OutFile $env:temp\winget.appxbundle
+            echo 'We download WinGet'
+            Start-Sleep -Seconds 3
+            Add-AppxPackage $env:temp\winget.appxbundle
+            clear | echo 'Installing software...'
+            InstallSoftware
+            Host-clear
+        }
 }
